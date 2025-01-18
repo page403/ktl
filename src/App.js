fa7e861d22e6f5acf5936afe405168db4c8125cd
Wahid <wahid@Wahids-MacBook-Pro.local>
2025-01-19 00:00:24 +0700

    adding the images

--- src/App.js
+++ src/App.js
@@ -12,229 +12,229 @@ const products = [
   {
     "id": 2,
     "title": "3D GUMMY DUCKLING 12X12X15GR",
-    "image": "",
+    "image": "/images/2.png",
     "category": "all"
   },
   {
     "id": 3,
     "title": "3D GUMMY ICE CONE 12X12X15GR",
-    "image": "",
+    "image": "/images/3.jpeg",
     "category": "all"
   },
   {
     "id": 4,
     "title": "3D GUMMY ORANGE 12X12X15GR",
-    "image": "",
+    "image": "/images/4.jpg",
     "category": "all"
   },
   {
     "id": 5,
     "title": "3D GUMMY STRAWBERRY PILLOWS 12X12X15GR",
-    "image": "",
+    "image": "images/5.jpeg",
     "category": "all"
   },
     {
     "id": 6,
     "title": "3D HAPPY BEAR 12X12X15GR",
-    "image": "",
+    "image": "/images/bear.png",
     "category": "all"
   },
   {
     "id": 7,
     "title": "BLEZZ GULAS",
-    "image": "",
+    "image": "/images/blez.jpeg",
     "category": "all"
   },
   {
     "id": 8,
     "title": "BLEZZ MANGGA MADU",
-    "image": "",
+    "image": "images/blez-mangga.jpeg",
     "category": "all"
   },
   {
     "id": 9,
     "title": "DENEX BISCUIT STICK CHOCOLATE WHIT BOX 33GRX6",
-    "image": "",
+    "image": "/images/denex.jpeg",
     "category": "all"
   },
   {
     "id": 10,
     "title": "DENEX BISCUIT STICK STRAWBERRY WHIT BOX 33GRX6",
-    "image": "",
+    "image": "/images/denex-stick-cklt.jpeg",
     "category": "all"
   },
   {
     "id": 11,
     "title": "DENEX FUNIMAL CHOCOLATE 28GR X 30",
-    "image": "",
+    "image": "/images/funimal-cklt.png",
     "category": "all"
   },
   {
     "id": 12,
     "title": "DENEX FUNIMAL STRAWBERRY 28GR X 30P",
-    "image": "",
+    "image": "/images/funimal-van.webp",
     "category": "all"
   },
   {
     "id": 13,
     "title": "DENEX FUNIMAL VANILA 28GR X 30P",
-    "image": "",
+    "image": "/images/fun-van.webp",
     "category": "all"
   },
   {
     "id": 14,
     "title": "DENEX HANDLE BISCUITS ASSORTED 350GRX6",
-    "image": "",
+    "image": "/images/Denex_Assorted_350gr_Bucket.webp",
     "category": "all"
   },
   {
     "id": 15,
     "title": "DENEX HANDLE BISCUITS BLACK COKIES 350GRX6",
-    "image": "",
+    "image": "/images/Denex_Black_Cookies_350gr_Bucket.webp",
     "category": "all"
   },
     {
     "id": 16,
     "title": "DENEX HANDLE BISCUITS BUTTER COKIES 350GRX6",
-    "image": "",
+    "image": "/images/Denex_Butter_Cookies_350gr_Bucket1.webp",
     "category": "all"
   },
   {
     "id": 17,
     "title": "DENEX HANDLE BISCUITS CHOCOLATE 350GRX6",
-    "image": "",
+    "image": "/images/Denex_Chocolate_Sandwich_350gr_Bucket_V2.webp",
     "category": "all"
   },
   {
     "id": 18,
     "title": "DENEX HANDLE BISCUITS GOLDEN 350GRX6",
-    "image": "",
+    "image": "/images/Denex_Golden_Cookies_350gr_Bucket_V2.webp",
     "category": "all"
   },
   {
     "id": 19,
     "title": "DENEX HANDLE BISCUITS SANDWICH VANILA 350GRX6",
-    "image": "",
+    "image": "/images/Denex_Vanilla_Sandwich_350gr_Bucket.webp",
     "category": "all"
   },
   {
     "id": 20,
     "title": "DENEX HANDLE BISCUITS STR & CHERRY7 350GRX6",
-    "image": "",
+    "image": "/images/Denex_Strawberry_and_Cherry_Cookies_350gr_Bucket.webp",
     "category": "all"
   },
   {
     "id": 21,
     "title": "DENEX HANDLE WF CREAM ASSORTED 300GRX6",
-    "image": "",
+    "image": "/images/Denex_Wafer_Cream_300gr_Assorted_Bucket.webp",
     "category": "all"
   },
     {
     "id": 22,
     "title": "DENEX HANDLE WF CREAM CHOCOLATE 300GRX6",
-    "image": "",
+    "image": "/images/Denex_Wafer_Cream_300gr_Chocolate_Bucket.webp",
     "category": "all"
   },
     {
     "id": 23,
     "title": "DENEX HANDLE WF CREAM SAPPUCCINO 300GRX6",
-    "image": "",
+    "image": "/images/Denex_Wafer_Cream_300gr_Cappuccino_Bucket.webp",
     "category": "all"
   },
     {
     "id": 24,
     "title": "DENEX HANDLE WF ROLL ASSORTED 300GRX6",
-    "image": "",
+    "image": "/images/Denex_Wafer_Stick_300gr_Assorted_Bucket.webp",
     "category": "all"
   },
     {
     "id": 25,
     "title": "DENEX HANDLE WF ROLL CAPPUCCINO 300GRX6",
-    "image": "",
+    "image": "/images/Denex_Wafer_Stick_300gr_Cappuccino_Bucket.webp",
     "category": "all"
   },
     {
     "id": 26,
     "title": "DENEX HANDLE WF ROLL CHOCOLATE 300GRX6",
-    "image": "",
+    "image": "/images/Denex_Wafer_Stick_300gr_Chocolate_Bucket.webp",
     "category": "all"
   },
     {
     "id": 27,
     "title": "DENEX HANDLE WF STICK BOXES CHOCOLATE 180GRX12",
-    "image": "",
+    "image": "/images/cklt.jpg",
     "category": "all"
   },
     {
     "id": 28,
     "title": "DENEX HANDLE WF STICK BOXES TIRAMISU 180GRX12",
-    "image": "",
+    "image": "/images/tiram.jpg",
     "category": "all"
   },
     {
     "id": 29,
     "title": "DENEX MILK COCONUT ORANGE 20GR",
-    "image": "",
+    "image": "/images/m.jpg",
     "category": "all"
   },
     {
     "id": 30,
     "title": "DENEX MINI BLACK CRACKERS 20GRX10X6RTG",
-    "image": "",
+    "image": "/images/Mini_Black_Crackers_25gr.webp",
     "category": "all"
   },
     {
     "id": 31,
     "title": "DENEX MINI CRACKERS 20GRX10X6RTG",
-    "image": "",
+    "image": "/images/Mini_Crackers_25gr.webp",
     "category": "all"
   },
     {
     "id": 32,
     "title": "DENEX POTATO 250 GR X 6",
-    "image": "",
+    "image": "/images/pot.jpeg",
     "category": "all"
   },
     {
     "id": 33,
     "title": "DENEX VEGETABLE & CHEESE STICK 50GRX40P",
-    "image": "",
+    "image": "/images/Denex_Vegetable_and_Cheese_Stick_50gr.webp",
     "category": "all"
   },
     {
     "id": 34,
     "title": "DRAGONFLY WHEAT FLOUR 10X1KG",
-    "image": "",
+    "image": "/images/drag1.webp",
     "category": "all"
   },
     {
     "id": 35,
     "title": "DRAGONFLY WHEAT FLOUR 40X500G",
-    "image": "",
+    "image": "/images/drag500.jpeg",
     "category": "all"
   },
     {
     "id": 36,
     "title": "FILOO BALLS BISCUIT CHOCOBALL",
-    "image": "",
+    "image": "/images/filoo-balls.jpg",
     "category": "all"
   },
     {
     "id": 37,
     "title": "FILOO CUBES CHOCOMILK SCH 6X10X1BG",
-    "image": "",
+    "image": "/images/filoo-cubes-rcg.jpg",
     "category": "all"
   },
     {
     "id": 38,
     "title": "FILOO CUBES CHOCOMILK TPL 12*50*3GR",
-    "image": "",
+    "image": "/images/filoo-cubes-tpl.jpg",
     "category": "all"
   },
     {
     "id": 39,
     "title": "FILOO SOES CHOCO CREAM 6RCG*10PCS*25GR",
-    "image": "",
+    "image": "/images/filoo-soes.png",
     "category": "all"
   }
 ];
