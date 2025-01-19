672fbdb322ecf68ea3d3863034eecfc7539a6576
Wahid <wahid@Wahids-MacBook-Pro.local>
2025-01-19 10:34:53 +0700

    adding top navbar

--- src/App.js
+++ src/App.js
@@ -7,259 +7,259 @@ const products = [
     "id": 1,
     "title": "3D FUN GUM GUMMY CANDIES 12X12X15GR",
     "image": "/images/1.jpeg",
-    "category": "all"
+    "category": "bites-gummyworld"
   },
   {
     "id": 2,
     "title": "3D GUMMY DUCKLING 12X12X15GR",
     "image": "/images/2.png",
-    "category": "all"
+    "category": "bites-gummyworld"
   },
   {
     "id": 3,
     "title": "3D GUMMY ICE CONE 12X12X15GR",
     "image": "/images/3.jpeg",
-    "category": "all"
+    "category": "bites-gummyworld"
   },
   {
     "id": 4,
     "title": "3D GUMMY ORANGE 12X12X15GR",
     "image": "/images/4.jpg",
-    "category": "all"
+    "category": "bites-gummyworld"
   },
   {
     "id": 5,
     "title": "3D GUMMY STRAWBERRY PILLOWS 12X12X15GR",
     "image": "images/5.jpeg",
-    "category": "all"
+    "category": "bites-gummyworld"
   },
     {
     "id": 6,
     "title": "3D HAPPY BEAR 12X12X15GR",
     "image": "/images/bear.png",
-    "category": "all"
+    "category": "bites-gummyworld"
   },
   {
     "id": 7,
     "title": "BLEZZ GULAS",
     "image": "/images/blez.jpeg",
-    "category": "all"
+    "category": "yafindo"
   },
   {
     "id": 8,
     "title": "BLEZZ MANGGA MADU",
     "image": "images/blez-mangga.jpeg",
-    "category": "all"
+    "category": "yafindo"
   },
   {
     "id": 9,
     "title": "DENEX BISCUIT STICK CHOCOLATE WHIT BOX 33GRX6",
     "image": "/images/denex.jpeg",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 10,
     "title": "DENEX BISCUIT STICK STRAWBERRY WHIT BOX 33GRX6",
     "image": "/images/denex-stick-cklt.jpeg",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 11,
     "title": "DENEX FUNIMAL CHOCOLATE 28GR X 30",
     "image": "/images/funimal-cklt.png",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 12,
     "title": "DENEX FUNIMAL STRAWBERRY 28GR X 30P",
     "image": "/images/funimal-van.webp",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 13,
     "title": "DENEX FUNIMAL VANILA 28GR X 30P",
     "image": "/images/fun-van.webp",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 14,
     "title": "DENEX HANDLE BISCUITS ASSORTED 350GRX6",
     "image": "/images/Denex_Assorted_350gr_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 15,
     "title": "DENEX HANDLE BISCUITS BLACK COKIES 350GRX6",
     "image": "/images/Denex_Black_Cookies_350gr_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 16,
     "title": "DENEX HANDLE BISCUITS BUTTER COKIES 350GRX6",
     "image": "/images/Denex_Butter_Cookies_350gr_Bucket1.webp",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 17,
     "title": "DENEX HANDLE BISCUITS CHOCOLATE 350GRX6",
     "image": "/images/Denex_Chocolate_Sandwich_350gr_Bucket_V2.webp",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 18,
     "title": "DENEX HANDLE BISCUITS GOLDEN 350GRX6",
     "image": "/images/Denex_Golden_Cookies_350gr_Bucket_V2.webp",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 19,
     "title": "DENEX HANDLE BISCUITS SANDWICH VANILA 350GRX6",
     "image": "/images/Denex_Vanilla_Sandwich_350gr_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 20,
     "title": "DENEX HANDLE BISCUITS STR & CHERRY7 350GRX6",
     "image": "/images/Denex_Strawberry_and_Cherry_Cookies_350gr_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
   {
     "id": 21,
     "title": "DENEX HANDLE WF CREAM ASSORTED 300GRX6",
     "image": "/images/Denex_Wafer_Cream_300gr_Assorted_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 22,
     "title": "DENEX HANDLE WF CREAM CHOCOLATE 300GRX6",
     "image": "/images/Denex_Wafer_Cream_300gr_Chocolate_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 23,
     "title": "DENEX HANDLE WF CREAM SAPPUCCINO 300GRX6",
     "image": "/images/Denex_Wafer_Cream_300gr_Cappuccino_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 24,
     "title": "DENEX HANDLE WF ROLL ASSORTED 300GRX6",
     "image": "/images/Denex_Wafer_Stick_300gr_Assorted_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 25,
     "title": "DENEX HANDLE WF ROLL CAPPUCCINO 300GRX6",
     "image": "/images/Denex_Wafer_Stick_300gr_Cappuccino_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 26,
     "title": "DENEX HANDLE WF ROLL CHOCOLATE 300GRX6",
     "image": "/images/Denex_Wafer_Stick_300gr_Chocolate_Bucket.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 27,
     "title": "DENEX HANDLE WF STICK BOXES CHOCOLATE 180GRX12",
     "image": "/images/cklt.jpg",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 28,
     "title": "DENEX HANDLE WF STICK BOXES TIRAMISU 180GRX12",
     "image": "/images/tiram.jpg",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 29,
     "title": "DENEX MILK COCONUT ORANGE 20GR",
     "image": "/images/m.jpg",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 30,
     "title": "DENEX MINI BLACK CRACKERS 20GRX10X6RTG",
     "image": "/images/Mini_Black_Crackers_25gr.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 31,
     "title": "DENEX MINI CRACKERS 20GRX10X6RTG",
     "image": "/images/Mini_Crackers_25gr.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 32,
     "title": "DENEX POTATO 250 GR X 6",
     "image": "/images/pot.jpeg",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 33,
     "title": "DENEX VEGETABLE & CHEESE STICK 50GRX40P",
     "image": "/images/Denex_Vegetable_and_Cheese_Stick_50gr.webp",
-    "category": "all"
+    "category": "denex"
   },
     {
     "id": 34,
     "title": "DRAGONFLY WHEAT FLOUR 10X1KG",
     "image": "/images/drag1.webp",
-    "category": "all"
+    "category": "harvestar"
   },
     {
     "id": 35,
     "title": "DRAGONFLY WHEAT FLOUR 40X500G",
     "image": "/images/drag500.jpeg",
-    "category": "all"
+    "category": "harvestar"
   },
     {
     "id": 36,
     "title": "FILOO BALLS BISCUIT CHOCOBALL",
     "image": "/images/filoo-balls.jpg",
-    "category": "all"
+    "category": "yafindo"
   },
     {
     "id": 37,
     "title": "FILOO CUBES CHOCOMILK SCH 6X10X1BG",
     "image": "/images/filoo-cubes-rcg.jpg",
-    "category": "all"
+    "category": "yafindo"
   },
     {
     "id": 38,
     "title": "FILOO CUBES CHOCOMILK TPL 12*50*3GR",
     "image": "/images/filoo-cubes-tpl.jpg",
-    "category": "all"
+    "category": "yafindo"
   },
     {
     "id": 39,
     "title": "FILOO SOES CHOCO CREAM 6RCG*10PCS*25GR",
     "image": "/images/filoo-soes.png",
-    "category": "all"
+    "category": "yafindo"
   },
   {
     "id": 40,
     "title": "FUNNY BEAR BURGER 12X12X15GR",
     "image": "/images/funny-b.jpg",
-    "category": "all"
+    "category": "bites-gummyworld"
   },
   {
     "id": 41,
     "title": "FUNNY BEAR PIZZA 12X12X15GR",
     "image": "/images/funny-p.jpg",
-    "category": "all"
+    "category": "bites-gummyworld"
   },
   {
     "id": 42,
     "title": "JA MONA KOPYOR 20X300G",
     "image": "/images/mona.jpg",
-    "category": "all"
+    "category": "khong-guan"
   },
   {
     "id": 43,
     "title": "JB BISTAR BLUBERI 12X10X16G",
     "image": "/images/bistar-blue.webp",
-    "category": "all"
+    "category": "khong-guan"
   },
   {
     "id": 44,
@@ -271,37 +271,37 @@ const products = [
     "id": 45,
     "title": "JB BISTAR STROBERI 12X10X16G",
     "image": "/images/bistar-stro.webp",
-    "category": "all"
+    "category": "khong-guan"
   },
   {
     "id": 46,
     "title": "JELLY MARICO ANGGUR",
     "image": "/images/anggur.png",
-    "category": "all"
+    "category": "yafindo"
   },
   {
     "id": 47,
     "title": "JELLY MARICO JAMBU",
     "image": "/images/jambu.jpg",
-    "category": "all"
+    "category": "yafindo"
   },
   {
     "id": 48,
     "title": "JELLY MARICO MANGGA",
     "image": "/images/mangga.jpg",
-    "category": "all"
+    "category": "yafindo"
   },
   {
     "id": 49,
     "title": "JELLY MARICO STRAWBERY",
     "image": "/images/marico-stro.jpg",
-    "category": "all"
+    "category": "yafindo"
   },
   {
     "id": 50,
     "title": "KG ASSORTED BISCUITS CLASSIC MINI 350GX6 TIN",
     "image": "/images/khong_guan_khong_guan_classic_assorted_350gr_full01_m621mm5k.jpg",
-    "category": "all"
+    "category": "khong-guan"
   }
 ];
 
@@ -338,23 +338,13 @@ function App() {
   return (
     <div className="app">
       <header className="header">
-        <h1>Catalogue</h1>
+        <a> </a>
+        <ul class="list">
+          <li><a href="#">CONTACT SALESMAN</a></li>
+          <li><a href="#">ABOUT</a></li>
+        </ul>
+      
       </header>
-      <nav className="category-nav">
-        <button
-          className={activeCategory === 'all' ? 'active' : ''}
-          onClick={() => setActiveCategory('all')}
-        >
-          All
-        </button>
-        <button
-          className={activeCategory === 'BKP' ? 'active' : ''}
-          onClick={() => setActiveCategory('BKP')}
-        >
-          BKP
-        </button>
-      </nav>
-
       <main className="product-list">
         {filteredProducts.map((product) => (
           <div key={product.id} className="product-card">
@@ -371,6 +361,9 @@ function App() {
           <h3>Options</h3>
           <button onClick={() => setActiveCategory('all')}>All</button>
           <button onClick={() => setActiveCategory('BKP')}>BKP</button>
+          <button onClick={() => setActiveCategory('bites-gummyworld')}>Bites Gummyworld</button>
+          <button onClick={() => setActiveCategory('yafindo')}>Yafindo</button>
+          <button onClick={() => setActiveCategory('khong-guan')}>Khong Guan</button>
           <button onClick={() => setPopupVisible(false)}>Close</button>
         </div>
       )}
